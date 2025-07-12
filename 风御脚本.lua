50de9bf37e64fa64712a59dcd83cb8a5f5704b9a
FengYu <15296258498@163.com>
2025-07-12 19:48:35 +0800

    Add files via upload

--- 风御脚本.lua
+++ 风御脚本.lua
@@ -1,143 +1,111 @@
-if getgenv().ED_AntiKick then
-	return
-end
+    repeat
+        task.wait()
+    until game:IsLoaded()
+    local TweenService = game:GetService("TweenService")
+local LocalizationService = game:GetService("LocalizationService")
+local LocalizationService = game:GetService("LocalizationService")
+local player = game.Players.LocalPlayer
 
-getgenv().ED_AntiKick = {
-	Enabled = true, -- Set to false if you want to disable the Anti-Kick.
-	SendNotifications = true, -- Set to true if you want to get notified for every event
-	CheckCaller = true -- Set to true if you want to disable kicking by other executed scripts
-}
-                warn("Anti afk running")
-game:GetService("Players").LocalPlayer.Idled:connect(function()
-warn("Anti afk ran")
-game:GetService("VirtualUser"):CaptureController()
-game:GetService("VirtualUser"):ClickButton2(Vector2.new())
-end)
-local drop
-local function dealerships()
-local tables = {"Dealerships"}
-for i,v in pairs(workspace.Etc.Dealership:GetChildren()) do
-    if v.ClassName == "Model" then
-    table.insert(tables,v.Name)
-end
-end
-return tables
-end
-getfenv().grav = workspace.Gravity
-local bai = {
-    axedupe = false,
-    soltnumber = "1",
-    waterwalk = false,
-    awaysday = false,
-    awaysdnight = false,
-    nofog = false,
-    axeflying = false,
-    playernamedied = "",
-    tptree = "",
-    moneyaoumt = 1,
-    moneytoplayername = "",
-    donationRecipient = tostring(game.Players.LocalPlayer),
-    autodropae = false,
-    farAxeEquip = nil,
-    cuttreeselect = "Generic",
-    autofarm = false,
-    PlankToBlueprint = nil,
-    plankModel = nil,
-    blueprintModel = nil,
-    saymege = "",
-    autosay = false,
-    saymount = 1,
-    sayfast = false,
-    autofarm1 = false,
-    bringamount = 1,
-    bringtree = false,
-    dxmz = "",
-    color = 0,
-    0,
-    0,
-    zlwjia = "",
-    mtwjia = nil,
-    zix = 1,
-    zlz = 3,
-    axeFling = nil,
-    itemtoopen = "",
-    openItem = nil,
-    car = nil,
-    load = false,
-    autobuyamount = 1,
-    autopick = false,
-    loaddupeaxewaittime = 3.1,
-    walkspeed = 16,
-    JumpPower = 50,
-    pickupaxeamount = 1,
-    whthmose = false,
-    itemset = nil,
-    LoneCaveAxeDetection = nil,
-    cuttree = false,
-    LoneCaveCharacterAddedDetection = nil,
-    LoneCaveDeathDetection = nil,
-    lovecavecutcframe = nil,
-    lovecavepast = nil,
-    zlmt = nil,
-    shuzhe = false,
-    modwood = false,
-    tchonmt = nil,
-    cskais = false,
-    dledetree = false,
-    delereeset = nil,
-    treecutset = nil,
-    autobuyset = nil,
-    wood = 7,
-    cswjia = nil,
-    boxOpenConnection = nil,
-    autobuystop = flase,
-    dropdown = {},
-    autocsdx = nil,
-    kuangxiu = nil,
-    xzemuban = false,
-    daiwp = false,
-    stopcar = false
-}
- 
-local dropdown = {}
-local playernamedied = ""
+local result, code = pcall(function()
+	return LocalizationService:GetCountryRegionForPlayerAsync(player)
+end)   
+local ScreenGui = Instance.new("ScreenGui")
+local MAIN = Instance.new("Frame")
+local LOGO = Instance.new("ImageLabel")
+local UICorner = Instance.new("UICorner")
+local BAR = Instance.new("Frame")
+local UICorner_2 = Instance.new("UICorner")
+local TITLE = Instance.new("TextLabel")
+local LOADING = Instance.new("TextLabel")
+local WindowStroke = Instance.new("UIStroke")
 
-for i, player in pairs(game.Players:GetPlayers()) do
-    dropdown[i] = player.Name
-end
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-function Notify(top, text, ico, dur)
-  game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = top,
-    Text = text,
-    Icon = ico,
-    Duration = dur,
-  })
-end
+MAIN.Name = "MAIN"
+MAIN.Parent = ScreenGui
+MAIN.BackgroundColor3 = Color3.fromRGB(4, 4, 4)
+MAIN.AnchorPoint = Vector2.new(0.5,0.5)
+MAIN.Position = UDim2.new(0.5, 0, 0.5, 0)
+MAIN.Size = UDim2.new(0, 357, 0, 158)
 
-getgenv().SheriffAim = false;
-getgenv().GunAccuracy = 25;
+WindowStroke.Name = "WindowStroke"
+WindowStroke.Parent = MAIN
+WindowStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+WindowStroke.Color = Color3.fromRGB(255,255,255)
+WindowStroke.LineJoinMode = Enum.LineJoinMode.Round
+WindowStroke.Thickness = 2
+WindowStroke.Transparency = 0
+WindowStroke.Enabled = true
+WindowStroke.Archivable = true
 
-local GunHook
-GunHook = hookmetamethod(game, "__namecall", function(self, ...)
-        local method = getnamecallmethod()
-        local args = { ... }
-        if not checkcaller() then
-                if typeof(self) == "Instance" then
-                        if self.Name == "ShootGun" and method == "InvokeServer" then
-                                if getgenv().SheriffAim and getgenv().GunAccuracy then
-                                        if Murderer then
-                                                local Root = Players[tostring(Murder)].Character.PrimaryPart;
-                                                local Veloc = Root.AssemblyLinearVelocity;
-                                                local Pos = Root.Position + (Veloc * Vector3.new(getgenv().GunAccuracy / 200, 0, getgenv().GunAccuracy/ 200));
-                                                args[2] = Pos;
-                                        end;
-                                end;
-                        end;
-                end;
-        end;
-        return GunHook(self, unpack(args));
-end);
+LOGO.Name = "LOGO"
+LOGO.Parent = MAIN
+LOGO.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+LOGO.BackgroundTransparency = 1.000
+LOGO.Position = UDim2.new(-0.0323287919, 0, -0.079336822, 0)
+LOGO.Size = UDim2.new(0, 162, 0, 160)
+LOGO.Image = "rbxassetid:/131231007815032"--11354741327
+
+UICorner.CornerRadius = UDim.new(0, 5)
+UICorner.Parent = MAIN
+
+BAR.Name = "BAR"
+BAR.Parent = MAIN
+BAR.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+BAR.BorderSizePixel = 0
+BAR.Position = UDim2.new(0.106, 0,0.835, 0)
+BAR.Size = UDim2.new(0, 0, 0, 15)
+
+UICorner_2.Parent = BAR
+
+TITLE.Name = "TITLE"
+TITLE.Parent = MAIN
+TITLE.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TITLE.BackgroundTransparency = 1.000
+TITLE.Position = UDim2.new(0.403277725, 0, 0.0506329127, 0)
+TITLE.Size = UDim2.new(0, 169, 0, 44)
+TITLE.Font = Enum.Font.Highway
+TITLE.Text = "殺脚本..."
+TITLE.TextColor3 = Color3.fromRGB(0, 0, 255)
+TITLE.TextScaled = true
+TITLE.TextSize = 14.000
+TITLE.TextWrapped = true
+
+LOADING.Name = "LOADING"
+LOADING.Parent = MAIN
+LOADING.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+LOADING.BackgroundTransparency = 1.000
+LOADING.Position = UDim2.new(0.378, 0,0.43, 0)
+LOADING.Size = UDim2.new(0, 169, 0, 44)
+LOADING.Font = Enum.Font.SourceSansItalic
+LOADING.Text = "加载中…"
+LOADING.TextColor3 = Color3.fromRGB(0, 0, 255)
+LOADING.TextSize = 20.000
+LOADING.TextWrapped = true
+wait(1)
+LOADING.Text = "获取数据..."
+BAR:TweenSize(UDim2.new(0, 150,0, 15), Enum.EasingDirection.Out, Enum.EasingStyle.Quart, .3, true)
+wait(1)
+LOADING.Text = "你好 " ..game.Players.LocalPlayer.DisplayName.. " ，来自: ".. code
+BAR:TweenSize(UDim2.new(0, 200,0, 15), Enum.EasingDirection.Out, Enum.EasingStyle.Quart, .3, true)
+wait(1)
+LOADING.Text = "检查游戏..."
+BAR:TweenSize(UDim2.new(0, 250,0, 15), Enum.EasingDirection.Out, Enum.EasingStyle.Quart, .3, true)
+wait(1)
+LOADING.Text = "欢迎使用殺脚本"
+BAR:TweenSize(UDim2.new(0, 260,0, 15), Enum.EasingDirection.Out, Enum.EasingStyle.Quart, .3, true)
+wait(1)
+LOADING.Text = "谢谢使用"
+BAR:TweenSize(UDim2.new(0, 285,0, 15), Enum.EasingDirection.Out, Enum.EasingStyle.Quart, .3, true)
+wait(1)
+MAIN:TweenSize(UDim2.new(0,0,0,5),"Out","Quad",1,true)
+LOGO:Destroy()
+TITLE:Destroy()
+LOADING:Destroy()
+BAR:Destroy()
+wait(1)
+MAIN:Destroy()
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/FengY4/FengY4/refs/heads/main/Fengui.lua", true))()
 ----------------------------------------------------------------------------------------------------------------------------------------
 local window = library:new("汽脚本")--V1
