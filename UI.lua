f2067df7d971f3020696d54ec664912bd881876b
Law-feng <15296258498@163.com>
2025-08-31 18:35:23 +0800

    Add files via upload

--- UI.lua
+++ UI.lua
@@ -372,6 +372,16 @@ function library.new(library, name, theme)
         end
     end
 
+    if theme then
+        for k, v in pairs(theme) do
+            if config[k] ~= nil then
+                config[k] = v
+            end
+        end
+    end
+
+    ScriptTitle.Text = name or "FengYu"
+
     local window = {}
     
     function window.Tab(window, name, icon)
