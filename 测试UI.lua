2accf9f6a3ac6e38b03152e939e386bcf078b03a
Law-feng <15296258498@163.com>
2025-10-14 13:03:48 +0800

    Add files via upload

--- 测试UI.lua
+++ 测试UI.lua
@@ -668,14 +668,14 @@ function FengY3.new(FengY3, name, theme)
     BtnModule.Parent = Objs
     BtnModule.BackgroundTransparency = 1
     BtnModule.BorderSizePixel = 0
-    BtnModule.Size = UDim2.new(0, 340, 0, 22)  -- 修改：宽度从330增加到340，高度从28减小到22
+    BtnModule.Size = UDim2.new(0, 300, 0, 35)  -- 修改：宽度从340减小到300，高度从22增加到35
     
     Btn.Name = "Btn"
     Btn.Parent = BtnModule
     Btn.BackgroundColor3 = config.Button_Color
     Btn.BackgroundTransparency = 0.2
     Btn.BorderSizePixel = 0
-    Btn.Size = UDim2.new(0, 340, 0, 22)  -- 修改：宽度从330增加到340，高度从28减小到22
+    Btn.Size = UDim2.new(0, 300, 0, 35)  -- 修改：宽度从340减小到300，高度从22增加到35
     Btn.AutoButtonColor = false
     Btn.Font = Enum.Font.GothamSemibold
     Btn.Text = "   " .. text
